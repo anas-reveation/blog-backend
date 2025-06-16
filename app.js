607e445b9e0c1043cfa23c851eb3297361ade130
anas khan <anas5khan0007@gmail.com>
2025-06-16 19:44:34 +0530

    add cors config

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express();
 const blogRoutes = require("./routes/blogRoutes");
 const authRoutes = require("./routes/authRoutes");
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", ""], // include both dev & deployed frontend URLs
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
