6480efe01e50adcd3919caa6cbbd6b84a1ba13b9
anas khan <anas5khan0007@gmail.com>
2025-06-17 17:19:18 +0530

    fix 4

--- app.js
+++ app.js
@@ -3,24 +3,12 @@ const cors = require("cors");
 const app = express();
 const blogRoutes = require("./routes/blogRoutes");
 const authRoutes = require("./routes/authRoutes");
-const allowedOrigins = [
-  "https://blog-frontend-xxfv.vercel.app",
-  "http://localhost:3000"
-];
 
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true); 
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS: " + origin));
-    }
-  },
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "Accept"],
-  credentials: true
 }));
+
 
 app.use(express.json());
 
